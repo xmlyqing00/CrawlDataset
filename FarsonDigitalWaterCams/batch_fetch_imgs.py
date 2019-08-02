cb8da94413d56d6461d286687bd539f04a70eb1f
LyqSpace <root@lyq.me>
2019-08-02 16:06:16 -0500

    change scripts to class. Add 10 locations.

--- FarsonDigitalWaterCams/batch_fetch_imgs.py
+++ FarsonDigitalWaterCams/batch_fetch_imgs.py
@@ -7,118 +7,158 @@ from urllib import request, parse, error
 from datetime import datetime, timezone
 from threading import Timer
 
-interval = 10
-dataset_folder = 'Ship01/Dataset/FarsonWater'
 
-img_lefts = []
-img_urls = []
-img_ids = []
-loc_paths = []
+class FarsonDownloader:
 
-def prepare_img_urls(loc_names):
-
-    img_url_pattern = re.compile('src="(.+)\?rid=(\d+)"')
-
-    common_url = 'https://www.farsondigitalwatercams.com/locations/render_event_response'
-    headers = {
-        'User-Agent': 'Mozilla/5.0'
-    }
-
-    for loc_name in loc_names:
-
-        parameters = {
-            'id': loc_name,
-            'child': 'live_image',
-            'si': 'false',
-            'source': 'tabs',
-            'type': 'ajax_child_display'
-        }
-        request_data = parse.urlencode(parameters)
-        url = common_url + '?' + request_data
-        req = request.Request(url, headers=headers, method='GET')
-
-        try:
-            response = request.urlopen(req)
-            html = response.read()
-            html = html.decode("utf-8")
-            match_res = img_url_pattern.findall(html)
-
-            if len(match_res) > 0:
-                img_url, img_id = match_res[0]
-                img_urls.append(img_url)
-                img_ids.append(int(img_id))
-            else:
-                print('Unable to fetch initial data at:', loc_name)
-
-        except error.HTTPError as e:
-            print(e.reason)
-
-
-def fetch_img(loc_id):
-    
-    if img_lefts[loc_id] > 0:
-        img_lefts[loc_id] -= 1
-        t = Timer(interval, fetch_img, [loc_id])
-        t.start()
-    
-    headers = {
-        'User-Agent': 'Mozilla/5.0',
-
-    }
-    img_url = img_urls[loc_id] + '?ref=' + str(img_ids[loc_id])
-
-    req = request.Request(img_url, headers=headers, method='GET')
-    try:
-        response = request.urlopen(req, timeout=10)
-        html = response.read()
-        file = open('D:/ISIC/data/' + dataset_name + '/' + image_info['name'] + '.jpg', 'wb')
-        file.write(html)
-        file.close()
-
-    except Exception as e:
-        print(e)
-        image_count -= 1
-
-
-def batch_fetch_imgs(loc_names, img_n, st_id=None):
-
-    print('Fetch images in the following %d locations:' % (len(loc_names)))
-    print('\t', loc_names)
-
-    if not os.path.exists(dataset_folder):
-        print('Create dataset folder at:', dataset_folder)
-        os.makedirs(dataset_folder)
-
-    prepare_img_urls(loc_names)
-    print(img_urls, img_ids)
-
-    for i in range(len(loc_names)):
+    def __init__(self, loc_names, dataset_folder, img_n, time_st, interval=10):
         
-        loc_paths[i] = os.path.join(dataset_folder, loc_names[i])
-        if not os.path.exists(loc_paths[i]):
-            print('Create location folder at:', loc_paths[i])
-            os.makedirs(loc_paths[i])
-            
-        img_lefts[i] = img_n
+        # Properties
+        self.loc_names = loc_names
+        self.dataset_folder = dataset_folder
+        self.img_n = img_n
+        self.interval = interval
 
-        t = Timer(interval, fetch_img, [i])
-        t.start()
+        # Computation data
+        self.img_lefts = []
+        self.img_urls = []
+        self.img_ids = []
+        self.loc_paths = []
+        self.referers = []
+
+        # Timezone
+        self.tz_london = pytz.timezone('Europe/London')
+        self.tz_local = tzlocal.get_localzone()
+
+        time_st_london = self.tz_london.localize(time_st)
+        time_st_local = time_st_london.astimezone(self.tz_local)
+        self.timestamp_st_local = int(time_st_local.timestamp())
+
+        print('Start Getting Image List.')
+        print('\t At London time:', time_st_london)
+        print('\t At local time:', time_st_local)
+
+        print('Fetch images in the following %d locations in the UK:' % (len(loc_names)))
+        print('\t', loc_names)
+        print('')
+
+        if not os.path.exists(dataset_folder):
+            print('Create dataset folder at:', dataset_folder)
+            os.makedirs(dataset_folder)
+        
+        for i in range(len(self.loc_names)):
+            
+            loc_path = os.path.join(self.dataset_folder, self.loc_names[i])
+            self.loc_paths.append(loc_path)
+            if not os.path.exists(loc_path):
+                print('Create location folder at:', loc_path)
+                os.makedirs(loc_path)
+
+            self.img_lefts.append(self.img_n)
+
+        print('')
+
+    def __del__(self):
+        print('FarsonDownloader ends.')
+
+    def prepare_img_urls(self):
+
+        img_url_pattern = re.compile('src="(.+)\?rid=(\d+)"')
+
+        common_url = 'https://www.farsondigitalwatercams.com/locations/render_event_response'
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+        }
+
+        for loc_name in self.loc_names:
+
+            parameters = {
+                'id': loc_name,
+                'child': 'live_image',
+                'si': 'false',
+                'source': 'tabs',
+                'type': 'ajax_child_display'
+            }
+            request_data = parse.urlencode(parameters)
+            url = common_url + '?' + request_data
+            req = request.Request(url, headers=headers, method='GET')
+            self.referers.append(url)
+
+            try:
+                response = request.urlopen(req)
+                html = response.read()
+                html = html.decode("utf-8")
+                match_res = img_url_pattern.findall(html)
+
+                if len(match_res) > 0:
+                    img_url, img_id = match_res[0]
+                    self.img_urls.append(img_url)
+                    self.img_ids.append(int(img_id))
+                else:
+                    print('Unable to fetch initial data at:', loc_name)
+
+            except error.HTTPError as e:
+                print(e.reason)
+
+
+    def fetch_img(self, loc_id):
+        
+        self.img_lefts[loc_id] -= 1
+        if self.img_lefts[loc_id] > 0:
+            t = Timer(self.interval, self.fetch_img, [loc_id])
+            t.start()
+        
+        if self.img_ids[loc_id] < self.timestamp_st_local:
+            self.img_lefts[loc_id] += 1
+            print('Sleep... left:', self.img_lefts[loc_id], '\tcur ts:', self.img_ids[loc_id], 'target ts:', self.timestamp_st_local)
+            
+        else:
+            headers = {
+                'Referer': self.referers[loc_id],
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
+            }
+            img_url = self.img_urls[loc_id] + '?ref=' + str(self.img_ids[loc_id])
+
+            time_local = datetime.fromtimestamp(self.img_ids[loc_id])
+            time_london = self.tz_local.localize(time_local).astimezone(self.tz_london)
+
+            print('Fetch %s, left: %d.' % (loc_names[loc_id], self.img_lefts[loc_id]), '\tLondon time:', time_london)
+
+            req = request.Request(img_url, headers=headers, method='GET')
+            try:
+                response = request.urlopen(req, timeout=3)
+                html = response.read()
+
+                file_name = time_london.strftime('%Y-%m-%d-%H-%M-%S')
+                img_path = os.path.join(self.loc_paths[loc_id], file_name + '.jpg')
+                file = open(img_path, 'wb')
+                file.write(html)
+                file.close()
+
+            except Exception as e:
+                print(e)
+
+        self.img_ids[loc_id] += self.interval
+
+
+    def batch_fetch_imgs(self):
+
+        self.prepare_img_urls()
+        print(self.img_urls, self.img_ids)
+
+        for i in range(len(self.loc_names)):
+            t = Timer(self.interval, self.fetch_img, [i])
+            t.start()
     
 
 if __name__ == '__main__':
 
-    london_tz = pytz.timezone('Europe/London')
-    local_tz = tzlocal.get_localzone()
+    loc_names = ['auldgirth', 'bewdley', 'cockermouth', 'evesham-lock', 'aberlour', 'keswick_greta', 'holmrook', 'worcester', 'galway-city', 'dublin']
+    dataset_folder = '/Ship01/Dataset/water/FarsonWater'
+    img_n = 200
+    time_st = datetime(2019, 8, 3, 8, 0, 0) # London time
+    interval = 180
 
-    time_st_london = london_tz.localize(datetime(2019, 8, 1, 22, 21, 0)) # London time
-    time_st_local = time_st_london.astimezone(local_tz)
-    timestamp_st_local = int(time_st_local.timestamp())
+    downloader = FarsonDownloader(loc_names, dataset_folder, img_n, time_st, interval)
+    downloader.batch_fetch_imgs()
 
-    print('Start Getting Image List.')
-    print('\t At London time:', time_st_london)
-    print('\t At local time:', time_st_local)
-
-    loc_names = ['turriff', 'bainbridge-yorebridge', 'henley-on-thames']
-    batch_fetch_imgs(loc_names=loc_names, img_n=100)
-
-    print('Finished.')
+    
